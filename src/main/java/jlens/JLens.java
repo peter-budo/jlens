86890d7f6ef8d12f1fda3b51a6521752e2c973e7
Peter Miklosko <peter.budo@gmail.com>
2011-11-08 13:30:47 +0000

    - First test on JDK7 I/O - Mac JDK still hasn't got support for these new features :(

--- src/main/java/jlens/JLens.java
+++ src/main/java/jlens/JLens.java
@@ -1,8 +1,37 @@
 package jlens;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.FileStore;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.attribute.UserDefinedFileAttributeView;
+import java.util.List;
+
 public class JLens {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
+        File image = new File("IMG_2075.CR2");
+        Path file = image.toPath();
+        FileStore store = null;
+        try {
+            store = Files.getFileStore(file);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        if (!store.supportsFileAttributeView(UserDefinedFileAttributeView.class)) {
+            System.err.format("UserDefinedFileAttributeView not supported on %s\n", store);
+            System.exit(-1);
 
+        }
+        UserDefinedFileAttributeView view = Files.getFileAttributeView(file, UserDefinedFileAttributeView.class);
+        try{
+            List<String> list = view.list();
+            for(String listElement : list){
+                System.out.println(listElement);
+            }
+        } catch (IOException e){
+            e.printStackTrace();
+        }
     }
 }
